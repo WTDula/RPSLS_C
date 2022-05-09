fb5f7d26e722e147aabdc23119f1925792d9adda
WTDula <wtdula@gmail.com>
2022-05-09 16:06:26 -0500

    created spock class, implemented necessary overrides

---
+++ Spock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class Spock : Gesture
+    {
+        public IDictionary<string, string> winsAgainst;
+
+        public Spock()
+        {
+            this.name = "Spock";
+            IDictionary<string, string> wins = new Dictionary<string, string>();
+            wins.Add("Scissors", "Spock smashes Scissors");
+            wins.Add("Rock", "Spock vaporizes Rock");
+            this.winsAgainst = wins;
+        }
+        public override bool DoIWin(Gesture otherGesture)
+        {
+            if (winsAgainst.ContainsKey(otherGesture.name))
+            {
+                Console.WriteLine(this.winsAgainst[otherGesture.name]);
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+    }
+}
