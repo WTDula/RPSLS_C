cc3b084fb55014e0ad9d8e9ef20535bf8629b4eb
WTDula <wtdula@gmail.com>
2022-05-09 16:18:12 -0500

    created GameObj class for later use

---
+++ GameObj.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class GameObj
+    {
+        Gesture[] RPSLSArray = new Gesture[5];
+
+        public void CreateGameObj()
+        {
+            this.RPSLSArray[0] = new Rock();
+            this.RPSLSArray[1] = new Paper();
+            this.RPSLSArray[2] = new Scissors();
+            this.RPSLSArray[3] = new Lizard();
+            this.RPSLSArray[4] = new Spock();
+        }
+    }
+}
