99a7c8d0ecfef3eb0bf2148497ea7e5905160e75
WTDula <wtdula@gmail.com>
2022-05-09 15:54:31 -0500

    created scissors class, made proper overrides

---
+++ Scissors.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class Scissors : Gesture
+    {
+        public IDictionary<string, string> winsAgainst;
+
+        public Scissors()
+        {
+            this.name = "Scissors";
+            IDictionary<string, string> wins = new Dictionary<string, string>();
+            wins.Add("Paper", "Scissors cuts Paper");
+            wins.Add("Lizard", "Scissors decapitates Lizard");
+            this.winsAgainst = wins;
+
+        }
+        public override bool DoIWin(Gesture otherGesture)
+        {
+            if (winsAgainst.ContainsKey(otherGesture.name))
+            {
+                Console.WriteLine(this.winsAgainst[otherGesture.name]);
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+    }
+}
