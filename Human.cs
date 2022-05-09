1d553f65317f8a71d30a2aa702c4f736f44fedf8
WTDula <wtdula@gmail.com>
2022-05-09 11:55:33 -0500

    created Player class, Human class, Program class

---
+++ Human.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class Human : Player
+    {
+        public static int ChooseGesture()
+        {
+            int input;
+            Console.WriteLine("Select the number of your gesture for player 2: ");
+            input = int.Parse(Console.ReadLine());
+            return input;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class Player
+    {
+        int score = 0;
+
+    }
+}
